fa71d28ebc2f933b86f7a4c3327a53f36f819e55
Sqyphen <tobias.fieldhouse@gmail.com>
2019-10-17 10:45:17 +0100

    adds production security (s3o etc)

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const dotenv = require('dotenv').config({
+	silent: process.env.NODE_ENV === 'production'
+});
 
 const package = require('./package.json');
 const debug = require('debug')(`${package.name}:index`);
+const s3o = require('@financial-times/s3o-middleware');
 const express = require('express');
 const path = require('path');
 const app = express();
+const helmet = require('helmet');
+const express_enforces_ssl = require('express-enforces-ssl');
+const bodyParser = require('body-parser');
 const topics = require('./routes/topics');
+
+const PORT = process.env.PORT;
+if (!PORT) {
+	throw new Error('ERROR: PORT not specified in env');
+	return;
+}
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(helmet());
+	app.enable('trust proxy');
+	app.use(express_enforces_ssl());
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use('/static', express.static('static'));
 
 // Routes
+app.use(s3o);
 app.use('/topic', topics);
 
 app.use('/', (req, res) => {
@@ -22,11 +44,6 @@ app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send('Something broke!');
 });
-
-const PORT = process.env.PORT;
-if (!PORT) {
-	throw new Error('ERROR: PORT not specified in env');
-}
 
 const server = app.listen(PORT, function() {
 	console.log('Server is listening on port', PORT);
