c64d103ddd1f70dd907726731353f29eef43248e
Sqyphen <tobias.fieldhouse@gmail.com>
2019-10-23 10:36:35 +0100

    add completed (but messy) multi-topic endpoint

--- README.md
+++ README.md
@@ -4,6 +4,23 @@ A Follow based recommendation algorithm
 
 Using topic cluster analysis, topic correlations data and topic follow/unfollow ratios - can we suggest relevant topics to follow (or retirve articles) for based on any given topic(s)?
 
+## Todo tasks
+
+-   Add cluster variation
+    -   Add parameters for using the 3, 5, 10 and 20 cluster versions
+-   Use ‘non-overlapping’ rather than ‘non-matching’
+-   Add description field to whole result, describe the process
+-   Re-replace topic variables with concepts
+-   Add summary for how these suggestions were suggested (good AI practice)
+-   Add exclude lists
+-   Show excludes to the result output
+-   Add verbose parameter
+    -   return version for all results or nothing
+-   Add which clusters the multiples come from
+-   More words in the error (which is also not an error)
+-   More words in the reason why no suggestions we returned
+-   Put the above into a to-do list and replace with meeting notes
+
 ## Setup
 
 -   Clone the `suggestr` repo
@@ -32,7 +49,3 @@ Using topic cluster analysis, topic correlations data and topic follow/unfollow 
 -   Compare the above two lists and identify the topics that do not appear in the correlated list
 -   Sort the discovered topics by their follow/unfollow ratio
 -   Return a list of sorted topics
-
-## Todo tasks
-
--   Add option to start search with multiple topics
--- routes/topics.js
+++ routes/topics.js
@@ -34,8 +34,6 @@ router.get('/', async (req, res) => {
 		.then((results) => {
 			data.results = results;
 
-			// also return combined results ordered by occurence (how many times it came up for seperate topics) and ratio
-
 			const combined_sorted_topic_results = results
 				.filter((entry) => {
 					if (entry.variables.nonMatchingTopicsSorted) {
@@ -59,6 +57,59 @@ router.get('/', async (req, res) => {
 			);
 			data.nonMatchingTopicsSortedCombinedClean = nonMatchingTopicsSortedCombinedClean;
 
+			// Rank returned topics if they appear more than once
+			// -----
+			const ranked = compressArray(nonMatchingTopicsSortedCombinedClean);
+
+			const rankedCombined = mergedResults.map((entry) => {
+				let matched_entry = ranked.filter((sub_entry) => {
+					if (sub_entry.value === entry.concept_name) {
+						return sub_entry;
+					}
+				});
+				entry.count = matched_entry[0].count;
+				return entry;
+			});
+			data.rankedCombined = rankedCombined;
+
+			// Remove duplicate topics
+			// -----
+			var uniqueArrayOfObjects = rankedCombined.filter(function(
+				obj,
+				index,
+				self
+			) {
+				return (
+					index ===
+					self.findIndex(function(t) {
+						return t['concept_name'] === obj['concept_name'];
+					})
+				);
+			});
+
+			data.uniqueArrayOfObjects = uniqueArrayOfObjects;
+
+			// Sort by count and then by ratio
+			// -----
+			const nonMatchingTopicsSorted = uniqueArrayOfObjects.sort(function(
+				vote1,
+				vote2
+			) {
+				if (vote1.count > vote2.count) return -1;
+				if (vote1.count < vote2.count) return 1;
+				if (vote1.ratio < vote2.ratio) return 1;
+				if (vote1.ratio > vote2.ratio) return -1;
+			});
+
+			data.nonMatchingTopicsSorted = nonMatchingTopicsSorted;
+
+			// Clean the final result to return only the topic names sorted
+			// -----
+			const nonMatchingTopicsSortedClean = nonMatchingTopicsSorted.map(
+				(entry) => entry.concept_name
+			);
+			data.nonMatchingTopicsSortedClean = nonMatchingTopicsSortedClean;
+
 			res.status(200).json(data);
 			return;
 		})
@@ -70,5 +121,34 @@ router.get('/', async (req, res) => {
 
 	return;
 });
+
+function compressArray(original) {
+	var compressed = [];
+	// make a copy of the input array
+	var copy = original.slice(0);
+
+	// first loop goes over every element
+	for (var i = 0; i < original.length; i++) {
+		var myCount = 0;
+		// loop over every element in the copy and see if it's the same
+		for (var w = 0; w < copy.length; w++) {
+			if (original[i] == copy[w]) {
+				// increase amount of times duplicate is found
+				myCount++;
+				// sets item to undefined
+				delete copy[w];
+			}
+		}
+
+		if (myCount > 0) {
+			var a = new Object();
+			a.value = original[i];
+			a.count = myCount;
+			compressed.push(a);
+		}
+	}
+
+	return compressed;
+}
 
 module.exports = router;
