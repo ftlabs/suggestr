f03cf8bab5efa3e81583591d5291890ec9ee6e51
Sqyphen <tobias.fieldhouse@gmail.com>
2019-10-24 15:57:55 +0100

    adds exclusion list option

--- routes/topics.js
+++ routes/topics.js
@@ -4,7 +4,7 @@ const suggestion = require('../module/suggestion');
 
 router.get('/single/:topicName', async (req, res) => {
 	const verbose = req.query.verbose ? req.query.verbose === 'true' : true;
-	const exclude = req.query.exclude ? req.query.exclude : [];
+	const exclude = req.query.exclude ? req.query.exclude.split(',') : [];
 	const result = await suggestion.concepts(
 		req.params.topicName,
 		verbose,
@@ -16,7 +16,7 @@ router.get('/single/:topicName', async (req, res) => {
 
 router.get('/multiple/', async (req, res) => {
 	const verbose = req.query.verbose ? req.query.verbose === 'true' : true;
-	const exclude = req.query.exclude ? req.query.exclude : [];
+	const exclude = req.query.exclude ? req.query.exclude.split(',') : [];
 	const topics = req.query.topics.split(',');
 	const result = await suggestion.concepts(topics, verbose, exclude);
 	res.status(200).json(result);
