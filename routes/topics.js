84e46a12861867cf6ca9455d0a9052efd68aa756
Sqyphen <tobias.fieldhouse@gmail.com>
2019-10-31 18:13:31 +0000

    consolidates defaults into a single object for clarity

--- routes/topics.js
+++ routes/topics.js
@@ -3,23 +3,24 @@ const router = express.Router();
 const suggestion = require('../module/suggestion');
 
 const supportedClusterSelections = ['3', '5', '10', '20'];
-
-const defaultClusterSelection = '20';
-const defaultConcepts = '';
-const defaultExcludes = [];
-const defaultType = 'topics';
-const defaultVerbosity = true;
+const defaults = {
+	clusterSelection: '20',
+	concepts: '',
+	excludes: [],
+	type: 'topics',
+	verbosity: true
+};
 
 router.get('/', async (req, res) => {
 	const searchParams = {
 		clusterSelection:
 			req.query.clusterSelection && supportedClusterSelections.includes(req.query.clusterSelection)
 				? req.query.clusterSelection
-				: defaultClusterSelection,
-		concepts: req.query.concepts ? req.query.concepts.split(',') : defaultConcepts,
-		exclude: req.query.exclude ? req.query.exclude.split(',') : defaultExcludes,
-		type: defaultType,
-		verbose: req.query.verbose ? req.query.verbose === 'true' : defaultVerbosity
+				: defaults.clusterSelection,
+		concepts: req.query.concepts ? req.query.concepts.split(',') : defaults.concepts,
+		exclude: req.query.exclude ? req.query.exclude.split(',') : defaults.excludes,
+		type: defaults.type,
+		verbose: req.query.verbose ? req.query.verbose === 'true' : defaults.verbosity
 	};
 	const result = await suggestion.concepts(searchParams);
 	res.status(200).json(result);
