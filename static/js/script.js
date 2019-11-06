1f5dca96fa70ae29b0e505e367edf24ffd3acaf3
Sqyphen <tobias.fieldhouse@gmail.com>
2019-11-06 11:30:40 +0000

    adds fix for the use of ampersands in the concept query string

--- static/js/script.js
+++ static/js/script.js
@@ -7,17 +7,24 @@ function updateURL(e) {
 	e.preventDefault();
 
 	var finalURL = '';
-	var urlPreffix = window.location.protocol + '//' + window.location.hostname + '/topics/';
-	var searchConcepts = 'concepts=' + urlBuilderForm.searchConcepts.value;
-	var excludeConcpets = 'exclude=' + urlBuilderForm.excludeConcpets.value;
+	var urlPreffix = urlPreffixGen('topics');
+	var searchConcepts = 'concepts=' + encodeURIComponent(urlBuilderForm.searchConcepts.value);
+	var excludeConcpets = 'exclude=' + encodeURIComponent(urlBuilderForm.excludeConcpets.value);
 	var verbose = 'verbose=' + urlBuilderForm.verbose.value;
 	var clusterSelection = 'clusterSelection=' + urlBuilderForm.clusterSelection.value;
 
-	finalURL = encodeURI(
-		urlPreffix + '?' + searchConcepts + '&' + excludeConcpets + '&' + verbose + '&' + clusterSelection
-	);
+	finalURL = urlPreffix + '?' + searchConcepts + '&' + excludeConcpets + '&' + verbose + '&' + clusterSelection;
 
 	urlBuilderOutput.href = finalURL;
 	urlBuilderOutput.innerHTML = finalURL;
 	return;
+}
+
+function urlPreffixGen(type) {
+	var protocol = window.location.protocol + '//';
+	var hostname = window.location.hostname;
+	var portStr = ':' + window.location.port;
+	var port = window.location.port ? portStr : '';
+	var path = '/' + type + '/';
+	return encodeURI(protocol + hostname + port + path);
 }
